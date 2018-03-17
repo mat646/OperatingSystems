5ed7c984a68cc7e7938c9f0c67dd2ca6f2489495
mateusz <mat646@gmail.com>
2018-03-17 16:43:21 +0100

    Refactor file manager.

--- SetTwo/file_manager.c
+++ SetTwo/file_manager.c
@@ -12,61 +12,85 @@
 
 void generate(file source) {
     char block[source.record_size];
-    int handle = open(source.file_name,O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+    int handle = open(source.file_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
     for (int i = 0; i < source.record_amount; ++i) {
-        for (int j = 0; j < source.record_size-1; ++j) {
-            block[j] = (char) ((rand() % 24) + 97);
+        for (int j = 0; j < source.record_size - 1; ++j) {
+            block[j] = (char) ((rand() % 23) + 97);
         }
-        block[source.record_size-1] ='\n';
+        block[source.record_size - 1] = '\n';
         write(handle, block, source.record_size);
     }
 
     close(handle);
 };
 
-void sort_sys(file source){
-    char blok[source.record_size];
-    char blok2[source.record_size];
-    int we = open(source.file_name, O_RDWR);
-    for (int i = 0; i < source.record_amount; ++i) {
-        for (int j = i; j < source.record_amount; ++j) {
-            lseek(we, i*source.record_size, SEEK_SET);
-            int li = read(we, blok, sizeof(blok));
-            lseek(we, j*source.record_size, SEEK_SET);
-            li = read(we, blok2, sizeof(blok));
-            if (blok[0] > blok2[0]) {
-                write(we, blok, li);
-                lseek(we, i*source.record_size, SEEK_SET);
-                write(we, blok2, li);
+void sort_sys(file source) {
+    char block1[source.record_size];
+    char block2[source.record_size];
+    int handle = open(source.file_name, O_RDWR, S_IRUSR | S_IWUSR);
+    for (int j = 0; j < source.record_amount; ++j) {
+        for (int i = 0; i < source.record_amount-1; ++i) {
+            lseek(handle, i * source.record_size, SEEK_SET);
+            read(handle, block1, sizeof(block1));
+            lseek(handle, (i+1) * source.record_size, SEEK_SET);
+            read(handle, block2, sizeof(block2));
+            if (block1[0] > block2[0]) {
+                lseek(handle, i * source.record_size, SEEK_SET);
+                write(handle, block2, sizeof(block2));
+                lseek(handle, (i+1) * source.record_size, SEEK_SET);
+                write(handle, block1, sizeof(block1));
             }
         }
     }
+
+    close(handle);
 };
 
-void sort_lib(file source){
+void sort_lib(file source) {
+    char block1[source.record_size];
+    char block2[source.record_size];
+    FILE *plik = fopen(source.file_name, "r+");
+    if (plik) {
+        for (int i = 0; i < source.record_amount; ++i) {
+            for (int j = 0; j < source.record_amount-1; ++j) {
+                fseek(plik, j * source.record_size, 0);
+                fread(block1, sizeof(char), source.record_size, plik);
+                fseek(plik, (j+1) * source.record_size, 0);
+                fread(block2, sizeof(char), source.record_size, plik);
+                if (block1[0] > block2[0]) {
+                    fseek(plik, j * source.record_size, 0);
+                    fwrite(block2, sizeof(char), source.record_size, plik);
+                    fseek(plik, (j+1) * source.record_size, 0);
+                    fwrite(block1, sizeof(char), source.record_size, plik);
+                }
+            }
+        }
+    }
 
+    fclose(plik);
 };
 
-void copy_sys(file source, file target){
-    char blok[1024];
-    int we, wy;
-    int liczyt;
-    we=open(source.file_name, O_RDONLY);
-    wy=open(target.file_name,O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
-    while((liczyt=read(we,blok,sizeof(blok)))>0)
-        write(wy,blok,liczyt);
+void copy_sys(file source, file target) {
+    char block[source.record_size];
+    int length;
+    int handle_source = open(source.file_name, O_RDONLY);
+    int handle_target = open(target.file_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    while ((length = read(handle_source, block, sizeof(block))) > 0)
+        write(handle_target, block, length);
+
+    close(handle_source);
+    close(handle_target);
 };
 
-void copy_lib(file source, file target){
-    char napis[1];
+void copy_lib(file source, file target) {
+    char napis[source.record_size];
     FILE *plik = fopen(source.file_name, "r");
     FILE *plik2 = fopen(target.file_name, "w");
-    if(plik && plik2)
-    {
-        for (int i = 0; i < source.record_amount * (source.record_size); ++i) {
-            fread(napis, sizeof(char), 1,plik);
-            fwrite(napis, sizeof(char), 1, plik2);
+    if (plik && plik2) {
+        for (int i = 0; i < source.record_amount; ++i) {
+            fread(napis, sizeof(char), source.record_size, plik);
+            fwrite(napis, sizeof(char), source.record_size, plik2);
         }
 
         fclose(plik);
--- SetTwo/main.c
+++ SetTwo/main.c
@@ -4,9 +4,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "file_manager.h"
 
 int main(int argc, char **argv) {
+
+    srand ( time(NULL) );
 
     if (argc == 1) {
         printf("Parametry wywolania:\n 1. nazwa pliku\n 2. rozmiar rekordu\n 3. liczba rekordow\n 4. typ funkcji\n");
@@ -24,10 +27,30 @@ int main(int argc, char **argv) {
     file1.record_size = record_size;
     file1.record_amount = record_amount;
 
+    char name2[20] = "test2";
+
+    file file2;
+    file2.file_name = name2;
+    file2.record_size = record_size;
+    file2.record_amount = record_amount;
+
+    char name3[20] = "test3";
+
+    file file3;
+    file3.file_name = name3;
+    file3.record_size = record_size;
+    file3.record_amount = record_amount;
+
+    //TODO time measurement
+
     generate(file1);
 
-    //copy_lib("we", "wy", record_size, record_amount);
+    copy_sys(file1, file2);
 
-    sort_sys(file1);
+    sort_sys(file2);
+
+    copy_lib(file1, file3);
+
+    sort_lib(file3);
 
 }
