5016f1d3935335f7a41ac542836c6959f39f7204
mateusz <mat646@gmail.com>
2018-03-17 17:55:17 +0100

    Describe Makefile.

--- SetTwo/main.c
+++ SetTwo/main.c
@@ -13,12 +13,12 @@ int main(int argc, char **argv) {
     srand ( time(NULL) );
 
     if (argc == 1) {
-        printf("Parametry wywolania:\n 1. nazwa pliku\n 2. rozmiar rekordu\n 3. liczba rekordow\n 4. typ funkcji\n");
+        printf("Parametry wywolania:\n 0. operacja\n 1. nazwa pliku/plikow\n 2. rozmiar rekordu\n 3. liczba rekordow\n 4. typ funkcji\n");
         return 0;
     }
 
     int record_size, record_amount;
-    char name[20], name_copy[20], action[10], type[5];
+    char name[20], name_copy[20], action[10], type[5], label[30];
     sscanf(argv[1], "%s", action);
     if(action[0] == 'c') {
         sscanf(argv[2], "%s", name);
@@ -30,72 +30,44 @@ int main(int argc, char **argv) {
         sscanf(argv[2], "%s", name);
         sscanf(argv[3], "%d", &record_size);
         sscanf(argv[4], "%d", &record_amount);
-        sscanf(argv[5], "%s", type);
+        if(action[0] != 'g')
+            sscanf(argv[5], "%s", type);
     }
-
-
 
     file file1;
     file1.file_name = name;
     file1.record_size = record_size;
     file1.record_amount = record_amount;
 
-    char name2[20] = "test2";
-
     file file2;
-    file2.file_name = name2;
+    file2.file_name = name_copy;
     file2.record_size = record_size;
     file2.record_amount = record_amount;
 
-    char name3[20] = "test3";
-
-    file file3;
-    file3.file_name = name3;
-    file3.record_size = record_size;
-    file3.record_amount = record_amount;
-
-
     Time time1 = start();
 
-    generate(file1);
+    if (action[0] == 'c') {
+        if (type[0] == 's') {
+            copy_sys(file1, file2);
+            sscanf("copying with sys:", "%s", label);
+        } else {
+            copy_lib(file1, file2);
+            sscanf("copying with lib:", "%s", label);
+        }
+    } else if(action[0] == 'g') {
+        generate(file1);
+        sscanf("creating file:", "%s", label);
+    } else if(action[0] == 's') {
+        if (type[0] == 's') {
+            sort_sys(file1);
+            sscanf("sorting with sys:", "%s", label);
+        } else {
+            sort_lib(file1);
+            sscanf("sorting with lib:", "%s", label);
+        }
+    }
 
     end(&time1);
 
-    print(time1, "Creating file:");
-
-
-    time1 = start();
-
-    copy_sys(file1, file2);
-
-    end(&time1);
-
-    print(time1, "copying with sys:");
-
-
-    time1 = start();
-
-    sort_sys(file2);
-
-    end(&time1);
-
-    print(time1, "sorting with sys:");
-
-
-    time1 = start();
-
-    copy_lib(file1, file3);
-
-    end(&time1);
-
-    print(time1, "copying with lib:");
-
-
-    time1 = start();
-
-    sort_lib(file3);
-
-    end(&time1);
-
-    print(time1, "sorting with lib:");
+    print(time1, label);
 }
---
+++ SetTwo/main_dir.c
@@ -0,0 +1,11 @@
+//
+// Created by Mateusz Sokol on 17.03.18.
+//
+
+#include <dirent.h>
+
+int main() {
+
+
+
+}
